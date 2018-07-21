ec124121ddab1710a1b020b452551746280b9840
AliakseiSemchankau <leshaxim@mail.ru>
2018-07-21 11:48:55 +0300

    update

--- communities/parameters.py
+++ communities/parameters.py
@@ -1,5 +1,5 @@
 access_token = 'abcd' # access token for vk. 
 version = '5.80' # version of vk api to use.
 database_address = 'community_database.txt' # filename to dump/load a database.
-community_id = 88104982 # id of community to explore.
+community_id = 137333745 # id of community to explore.
 members_to_proceed = 300
